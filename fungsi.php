de3b7429984507d08e7dec03c0db3a52fd1c199c
ThreeYas <triyasseptiyanto180992@gmail.com>
2022-03-10 09:29:03 +0700

    file fungsi.php berisi sebuah fungsi, kemudian dilakukan pemanggilan terhadap fungsi tersebut 3x.

---
+++ fungsi.php
@@ -0,0 +1,8 @@
+<?php
+    function kampus() {
+        echo "Universitas Trunojoyo Madura ";
+      }
+      echo kampus()."Universitas Trunojoyo Madura merupakan kelanjutan dari Universitas Bangkalan Madura (Unibang) <br>";
+      echo kampus()."Peresmiannya berlangsung pada tanggal 23 Juli 2001 <br>";
+      echo kampus()."merupakan satu-satunya Universitas Negeri Yang ada di Pulau Madura <br>";
+?>
